7bedbc98384020e6aad2a33fbc9317ca3f3611ad
Idris patan <mohamad.patan@mavs.uta.edu>
2017-08-13 22:23:37 -0500

    frst commit changed search_bar, video_list, video_list_item, the app can make api call and retrieve array elements and print video string based on the number of returned lements

--- src/index.js
+++ src/index.js
@@ -1,15 +1,46 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search'
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = "AIzaSyCVBqo2cDJxXfhGBr3yxG5DHuaCmXIPI1s";
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// Create a new class component this should produce some HTML
+class App extends Component {
+    constructor(props){
+        super(props);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+        this.state ={ videos:[] };
+
+        YTSearch ({key: API_KEY, term: 'surfboards'}, (videoData) => {
+            // if i named videoData as just videos then we can also say "this.setState({videos})
+            this.setState({videos:videoData});
+            console.log(videoData);
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoList videos ={this.state.videos}/>
+            </div>
+        );
+    }
+}
+
+
+// // Create a new functional component this should produce some HTML ####### Went from Functional Component to Class component the following code is for functional component
+// const App = () => {
+//     return (
+//     <div>
+//         <SearchBar />
+//     </div>
+//     );
+// }
+
+
+// Take this component's generated HTML and put it on the page (DOM)
+
+ReactDOM.render(<App />, document.querySelector('.container'));
